572fb7836b76faee30f8bc72d4312cc1defb791e
Hitesh Behera <bhiteshme74@gmail.com>
2024-08-07 12:28:33 +0530

    refractor

--- open_cli_cc.py
+++ open_cli_cc.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json  # Add this line to import the json module
 import google.generativeai as genai
 from youtube_transcript_api import YouTubeTranscriptApi
 from dotenv import load_dotenv
@@ -12,7 +13,7 @@ gemini_api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=gemini_api_key)
 
 @st.cache_data
-def fetch_questions(ytvideo_url, quiz_difficulty):
+def fetch_questions(youtube_video_url, quiz_difficulty):
 
     RESPONSE_JSON = {
         "questions" : [
@@ -50,7 +51,7 @@ def fetch_questions(ytvideo_url, quiz_difficulty):
     }
     
     PROMPT_TEMPLATE="""
-    Youtube Video URL: {ytvideo_url}
+    Youtube Video URL: {youtube_video_url}
     You are an expert in generating MCQ type quiz on the basis of the video content.
     Given the above youtube link, create a quiz based on the contents of the video; 3 multiple choice question keeping difficulty level as {quiz_difficulty}
     Make sure the questions are not repeated and check all the questions to be conforming the text as well.
@@ -59,9 +60,10 @@ def fetch_questions(ytvideo_url, quiz_difficulty):
 
     Here is the RESPONSE_JSON:
     {RESPONSE_JSON}
+    
     """
 
-    formatted_template = PROMPT_TEMPLATE.format(ytvideo_url=ytvideo_url, quiz_difficulty=quiz_difficulty, RESPONSE_JSON=RESPONSE_JSON)
+    formatted_template = PROMPT_TEMPLATE.format(youtube_video_url=youtube_video_url, quiz_difficulty=quiz_difficulty, RESPONSE_JSON=RESPONSE_JSON)
 
     model = genai.GenerativeModel('gemini-pro')
 
@@ -74,7 +76,7 @@ def fetch_questions(ytvideo_url, quiz_difficulty):
 def main():
     st.title("YT Qz")
 
-    ytvideo_url = st.text_input("Enter the Youtube video link: ")
+    youtube_video_url = st.text_input("Enter the Youtube video link: ")
     quiz_difficulty = st.selectbox("Select the quiz difficulty level: ", ["easy", "medium", "hard"])
 
     session_state = st.session_state
@@ -86,7 +88,7 @@ def main():
         session_state.quiz_generated = st.button("Generate Quiz")
 
     if session_state.quiz_generated:
-        questions = fetch_questions(ytvideo_url=ytvideo_url, quiz_difficulty=quiz_difficulty)
+        questions = fetch_questions(youtube_video_url=youtube_video_url, quiz_difficulty=quiz_difficulty)
 
     selected_options = []
     correct_answers = []
@@ -106,8 +108,8 @@ def main():
             st.write(f"You selected: {selected_option}")
             st.write(f"Correct answer: {correct_answer}")
             if selected_option == correct_answer:
-                marks += 1
-        st.write(f"Total Marks: {marks}")
+                total_marks += 1
+        st.write(f"Total Marks: {total_marks}")
 
 if __name__ == "__main__":
     main()
